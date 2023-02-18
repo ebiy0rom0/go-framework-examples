2509bc6b99b9c80ba8e4ca973aaa8ad744fc3d0b
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-02-18 17:23:46 +0900

    [add]db initialize

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"examples/framework"
+	"examples/infra"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run() error {
+	var fw framework.Framework
+	flag.StringVar((*string)(&fw), "framework", "", "exec framework")
+	flag.StringVar((*string)(&fw), "f", "gin", "exec framework")
+
+	if err := infra.InitializeDb("db/example.db"); err != nil {
+		return err
+	}
+	return framework.Start(fw)
+}
